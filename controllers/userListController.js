638cc11ae70752e9c791aa2e3cbdc4d1ae0ee9cf
Jatin Ramchandani <jatinramchandani15@gmail.com>
2024-09-05 17:16:44 +0530

    added users list api

---
+++ controllers/userListController.js
@@ -0,0 +1,20 @@
+const pool = require("../db/db-config");
+
+const userListController = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    if (!userId) return res.status(400).json({ error: "Invalid Request" });
+
+    const users = await pool.query(
+      `SELECT id, name, email FROM users WHERE id!=$1`,
+      [userId]
+    );
+
+    return res.status(200).json({ users: users.rows });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = userListController;
