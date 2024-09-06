573b4be6a749a5dbeca3d9bcfc59ce73bdd79407
Jatin Ramchandani <jatinramchandani15@gmail.com>
2024-09-06 19:16:36 +0530

    added logs router

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -4,10 +4,13 @@ const app = express();
 const userRoutes = require("./userRoutes");
 const refreshTokenRoutes = require("./refreshTokenRoutes");
 const messageRouter = require("./messageRoutes");
+const logsRouter = require("./logsRoutes");
+
 const jwtAuthenticationMiddleware = require("../middlewares/jwtMiddlewares/jwtAuthenticationMiddleware");
 
 app.use("/users/", userRoutes);
 app.use("/token/", refreshTokenRoutes);
+app.use("/logs/", jwtAuthenticationMiddleware, logsRouter);
 app.use("/messages", jwtAuthenticationMiddleware, messageRouter);
 
 module.exports = app;
