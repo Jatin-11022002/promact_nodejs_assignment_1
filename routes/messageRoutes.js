13474a3cc740f13e3f3cce239564cbb80f9fb1dd
Jatin Ramchandani <jatinramchandani15@gmail.com>
2024-09-06 12:34:18 +0530

    added getMessage route

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,8 +3,9 @@ const messageRouter = express.Router();
 const sendMessageController = require("../controllers/messageControllers/sendMessageController");
 const editMessageController = require("../controllers/messageControllers/editMessageController");
 const deleteMessageController = require("../controllers/messageControllers/deleteMessageController");
+const getMessagesController = require("../controllers/messageControllers/getMessagesController");
 
-messageRouter.route("/").post(sendMessageController);
+messageRouter.route("/").post(sendMessageController).get(getMessagesController);
 messageRouter.route("/:messageId").post(editMessageController);
 messageRouter.route("/:messageId").delete(deleteMessageController);
 
