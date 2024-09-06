6cf9e80315355e2f90a58bf311d1960dedd184e1
Jatin Ramchandani <jatinramchandani15@gmail.com>
2024-09-06 19:22:45 +0530

    added logs middleware in route

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const express = require("express");
 const app = express();
 const serverPort = process.env.SERVER_PORT;
 const cookieParser = require("cookie-parser");
+const requestLoggingMiddleware = require("./middlewares/requestLogMiddleware/requestLogMiddleware");
 
 const routes = require("./routes/mainRoute");
 
+app.set("trust proxy", true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/api/", routes);
+
+app.use("/api/", requestLoggingMiddleware, routes);
 
 app.listen(serverPort, () => {
   console.log(`Server Listening on ${serverPort}`);
